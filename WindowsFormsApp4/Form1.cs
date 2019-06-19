c5e0d1f5b9ef9e0cd9fd3622b28b6592ac4d89e1
yaotianen <yaolichao@qq.com>
2019-06-19 10:32:37 +0800

    one

--- WindowsFormsApp4/Form1.cs
+++ WindowsFormsApp4/Form1.cs
@@ -15,5 +15,14 @@ namespace WindowsFormsApp4
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void test()
+        {
+
+        }
     }
 }
